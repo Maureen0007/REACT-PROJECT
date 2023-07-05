4b4d5b4e64bae53f6c02dcdfa36ac5ac2d4f9d49
Maureen <maureenwanjiku85@gmail.com>
2023-07-05 20:53:42 +0300

    set the navbar and add logo

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from '../assests/logo.jpg'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='leftside'>
+            <img src={Logo} />
+        </div>
+        <div className='rightside'></div>
+
+    </div>
+  )
+}
+
+export default Navbar
